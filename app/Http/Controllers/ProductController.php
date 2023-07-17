a6357e1f92c9b3829e2f70b937028d76c289e12c
Ricardo Martins <hikfm2@gmail.com>
2023-07-16 23:24:33 -0300

    feat: update anda delete products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\{CreateProductRequest, RetrieveProductRequest};
+use App\Http\Requests\{CreateProductRequest, RetrieveProductRequest, UpdateProductRequest};
 use App\Http\Services\ProductsServices\CreateProductService;
+use App\Http\Services\ProductsServices\DeleteProductService;
 use App\Http\Services\ProductsServices\RetrieveAllProductsService;
 use App\Http\Services\ProductsServices\RetrieveProductService;
+use App\Http\Services\ProductsServices\UpdateProductService;
 
 class ProductController extends Controller {
     public function create(CreateProductRequest $request) {
@@ -22,6 +24,18 @@ class ProductController extends Controller {
 
     public function retrieveProduct($id) {
         $allProducts = new RetrieveProductService();
+
+        return $allProducts->execute($id);
+    }
+
+    public function updateProduct(UpdateProductRequest $request, $id) {
+        $allProducts = new UpdateProductService();
+
+        return $allProducts->execute($request->all(), $id);
+    }
+
+    public function deleteProduct($id) {
+        $allProducts = new DeleteProductService();
 
         return $allProducts->execute($id);
     }
